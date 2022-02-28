b915d7eda49a7c2fbb743563d1b50de4883cddd7
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-02-28 16:01:52 +0100

    Finish Project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import { Box, Steps } from "./components/Box.jsx";
+import { Box } from "./components/Box.jsx";
 import './styles/global.css'
 
 const tempMin = -20;
@@ -10,19 +10,71 @@ const heartMax = 180;
 const stepsMin = 0;
 const stepsMax = 50000;
 
-export function App() {
+
+
+export class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      water: 1.5,
+      heart: 120,
+      temperature: -10,
+      steps: 3000,
+    }
+  }
+  calculateWater = () =>{
+    // this.setState({water : 1.5})
+    let water = 1.5
+    let steps = this.state.steps
+    let heart = this.state.heart
+    let temperature = this.state.temperature
+    if(temperature > 20){
+      water += (temperature - 20) * 0.02;
+    }
+    if(heart > 120){
+      water += (heart - 120) * 0.0008;
+    }
+    if(steps > 10000){
+      water += (steps - 10000) * 0.00002;
+    }
+    
+    this.setState({water})
+    
+  }
+
+  onHeartChange = (e) => {
+    this.setState({heart : e.target.value})
+    this.calculateWater()
+  }
+
+  onStepsChange = (e) => {
+    this.setState({steps : e.target.value})
+    this.calculateWater()
+  }
+
+  onTemperatureChange = (e) =>{
+    this.setState({temperature : e.target.value})
+    this.calculateWater()
+  }
+
+
+
+  render() {
     return (
       <div className="container-fluid">
         <div className="row">
-        <Box color="black" icon="directions_walk" value="3000" unit="steps" />
-        <Box color="red" icon="favorite" value="120" unit="bpm"/>
-        <Box color="yellow" icon="wb_sunny" value="-10" unit="°C"/>
-        <Box color="#3A85FF" icon="local_drink" value="1.5" unit="L"/>
-        <p>Heart : {heartMin}</p>
-        <p>Temperature : {tempMin}</p>
-        <p>Steps : {stepsMin}</p>
+          {/* Water */}
+          <Box color="#3A85FF" icon="local_drink" value={this.state.water.toFixed(2)} unit="L" />
+          {/* Steps */}
+          <Box color="black" icon="directions_walk" value={this.state.steps} unit="steps" min={stepsMin} max={stepsMax} onChange={this.onStepsChange}/>
+          {/* Heart */}
+          <Box color="red" icon="favorite" value={this.state.heart} unit="bpm" min={heartMin} max={heartMax} onChange={this.onHeartChange} />
+          {/* Temperature */}
+          <Box color="yellow" icon="wb_sunny" value={this.state.temperature} unit="°C"  min={tempMin} max={tempMax} onChange={this.onTemperatureChange} />
         </div>
       </div>
     );
+  }
 }
 export default App;
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
-export function Box(props){
-    return <div className="box col-sm-3 col-6">
-        <span className="material-icons" style={{fontSize: "100px" , color: props.color, value: props.value, unit: props.unit}}>{props.icon}</span>
-    </div>
+
+export class Box extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+
+    render() {
+        if (this.props.icon !== "local_drink") {
+            return (
+                <div className="box col-sm-3 col-6">
+                    <span className="material-icons" style={{ fontSize: "100px", color: this.props.color }}>
+                        {this.props.icon}
+                    </span>
+                    <p>{this.props.value} {this.props.unit}</p>
+                    <input type="range" min={this.props.min} max={this.props.max} value={this.props.value} onInput={this.props.onChange} />
+                </div>
+            )
+        } else {
+            return (
+                <div className="box col-sm-3 col-6">
+                    <span className="material-icons" style={{ fontSize: "100px", color: this.props.color }}>
+                        {this.props.icon}
+                    </span>
+                    <p>{this.props.value} {this.props.unit}</p>
+                </div>
+            )
+        }
+
+
+    }
 }
